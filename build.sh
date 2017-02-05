0f10217f0a94469086d24fc8ec708044ffd1ae46
Michael Pershyn <michael.pershyn@gmail.com>
2017-02-05 23:23:53 +0100

    List package content after the build

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Build the binary debian packages for apache-storm
 pushd apache-storm
-
 dpkg-buildpackage -b -rfakeroot
-
 popd
+
+# Write down sample layouts for build packages
+for pkg in $(ls storm*.deb)
+do
+	dpkg -c $pkg > ./sample-layout/$(cut -d '_' -f 1 <<< $pkg).sample-layout.txt
+done
+
